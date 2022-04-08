22f1e7532696b316c48edba44144e447931dae00
Divya <dsatta@evoketechnologies.com>
2022-04-08 16:51:10 +0530

    Error correction

--- emp/EmpController.js
+++ emp/EmpController.js
@@ -399,7 +399,7 @@ router.post("/upload", function (req, res) {
                 const notMatchedHeaderItem = Object.keys(result[0]).filter((allNameObject) => !Object.keys(empHeaders).includes(allNameObject));
                 const matchedHeaderItem =    Object.keys(empHeaders).filter((allNameObject)=> !Object.keys(result[0]).includes(allNameObject));
                 
-                res.json({ error_code: 1, err_desc: ` These are the '${notMatchedHeaderItem}'  headers not matching with DB : Please enter headers as '${matchedHeaderItem}' respectively ` });
+                res.json({ error_code: 1, err_desc: ` These are the '${notMatchedHeaderItem}'  headers not matching with DB : Please enter headers as '${matchedHeaderItem}' respectively into the DB` });
   
               }
           } catch (e) {
